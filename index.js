c442bbc63c0b93a5c23532c4e0808a2b97eaab16
iamvarunnair <imvarunnair@gmail.com>
2021-02-17 22:58:15 +0530

    more minor changes

--- index.js
+++ index.js
@@ -53,5 +53,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running/`);
 });
