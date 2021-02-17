d9e514d5d2b13dcf95c4fa9503467bee2bfd738f
iamvarunnair <imvarunnair@gmail.com>
2021-02-17 22:31:54 +0530

    minor changes

--- index.js
+++ index.js
@@ -9,45 +9,45 @@ const server = http.createServer((req, res) => {
     res.end(`
             <!DOCTYPE html>
             <html lang="en">
-            <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Zzzzz...</title>
-            <style>
-                body {
-                    background: rgb(39, 39, 39);
-                    display: flex;
-                    flex-flow: column;
-                    justify-content: center;
-                    align-items: center;
-                }
-                h1 {
-                    font-size: 72px;
-                    font-family: Arial, Helvetica, sans-serif;
-                    background: linear-gradient(129deg, rgba(0,252,245,1) 0%, rgba(0,137,255,1) 100%);
-                    -webkit-text-fill-color: transparent;
-                    -webkit-background-clip: text;
-                }
-                h3 {
-                    font-size: 30px;
-                    font-family: Arial, Helvetica, sans-serif;
-                    background: linear-gradient(129deg, rgba(52,215,205,1) 0%, rgba(34,194,223,1) 52%, rgba(0,155,255,1) 100%);
-                    -webkit-text-fill-color: transparent;
-                    -webkit-background-clip: text;
-                }
-                span {
-                    font-size: 30px;
-                    font-family: Arial, Helvetica, sans-serif;
-                    background: linear-gradient(129deg, rgba(252,0,30,1) 0%, rgba(0,155,255,1) 100%);
-                    -webkit-text-fill-color: transparent;
-                    -webkit-background-clip: text;
-                }
-            </style>
-            </head>
-            <body>
-            <h1>Hello You!</h1>
-            <h3>This page is sleeping right now. <span>Visit later?</span></h3>
-            </body>
+                <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <title>Zzzzz...</title>
+                    <style>
+                        body {
+                            background: rgb(39, 39, 39);
+                            display: flex;
+                            flex-flow: column;
+                            justify-content: center;
+                            align-items: center;
+                        }
+                        h1 {
+                            font-size: 72px;
+                            font-family: Arial, Helvetica, sans-serif;
+                            background: linear-gradient(129deg, rgba(0,252,245,1) 0%, rgba(0,137,255,1) 100%);
+                            -webkit-text-fill-color: transparent;
+                            -webkit-background-clip: text;
+                        }
+                        h3 {
+                            font-size: 30px;
+                            font-family: Arial, Helvetica, sans-serif;
+                            background: linear-gradient(129deg, rgba(52,215,205,1) 0%, rgba(34,194,223,1) 52%, rgba(0,155,255,1) 100%);
+                            -webkit-text-fill-color: transparent;
+                            -webkit-background-clip: text;
+                        }
+                        span {
+                            font-size: 30px;
+                            font-family: Arial, Helvetica, sans-serif;
+                            background: linear-gradient(129deg, rgba(252,0,30,1) 0%, rgba(0,155,255,1) 100%);
+                            -webkit-text-fill-color: transparent;
+                            -webkit-background-clip: text;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <h1>Hello You!</h1>
+                    <h3>This page is sleeping right now. <span>Visit later?</span></h3>
+                </body>
             </html>
         `);
 });
